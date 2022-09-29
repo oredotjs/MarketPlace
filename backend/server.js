3a2cab74e2ddefb5b73922fc23de442e0cf9c37e
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-09-29 21:10:36 +0100

    :tada: initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-// const connectDb = require("./config/database");
-// dotenv.config({ path: "backend/config/config.env" });
+const connectDb = require("./config/database");
+dotenv.config({ path: "backend/config/config.env" });
 
-// connectDb();
+connectDb();
 
 const port = process.env.PORT || 3000;
 const mode = process.env.NODE_ENV;
